0d0338866286418aab7541c0485aec7fd64d5572
fusu <fusu@2dfire.com>
2017-04-06 18:21:04 +0800

    add epoll

--- longServer/src/EPoll.cpp
+++ longServer/src/EPoll.cpp
@@ -1,12 +1,35 @@
 #include "EPoll.h"
+#include <unistd.h>
 
 
 EPoll::EPoll()
 {
-
+    epollfd = epoll_create1(EPOLL_CLOEXEC);
+    if(epollfd == -1){
+        //...
+    }
+    event.events = EPOLLIN | EPOLLET; //边沿触发
+    //const int MAXEVENTS = 512;
+    events = new epoll_event[512];
 }
 
 EPoll::~EPoll()
 {
+    ::close(epollfd);
+}
 
+void EPoll::AddEpoll(int socketfd)
+{
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, socketfd, &event);
+}
+
+
+void EPoll::DelEpoll(int socketfd)
+{
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, socketfd, &event);
+}
+
+void EPoll::EpollWait()
+{
+    int nRead = epoll_wait(epollfd, )
 }
--- longServer/src/EPoll.h
+++ longServer/src/EPoll.h
@@ -6,9 +6,15 @@
 class EPoll
 {
 private:
-
+    int epollfd; //epoll文件描述符
+    struct epoll_event event;
+    struct epoll_event events[512];
 
 public:
     EPoll();
     ~EPoll();
+
+    void AddEpoll(int socketfd);
+    void DelEpoll(int socketfd);
+    void EpollWait();
 };

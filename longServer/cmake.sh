9430701e8b34454ccb26ad493a9ec4e1ac7c02b6
jacksoncy <wuxiangwei@yalla.live>
2022-11-02 14:45:16 +0800

    依赖更新

--- longServer/cmake.sh
+++ longServer/cmake.sh
@@ -3,21 +3,12 @@ set -u
 
 dir="build"
 
-if [ -d $dir ]; then
-    cmake --build $dir --config Release -j 4
-    exit 0
+if [ ! -d $dir ]; then
+	mkdir -p $dir
+
+	conan install . -if $dir -b missing -u
+
+	cmake -S . -B $dir -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 fi
 
-mkdir -p $dir
-
-cd $dir || exit 1
-
-conan install .. --build missing
-
-cmake -S .. -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_BUILD_TYPE=Release
-
 cmake --build . --config Release -j 4
-
-lnFile="compile_commands.json"
-
-ln -sf $dir/$lnFile .
--- longServer/include/utils/event_loop.hpp
+++ longServer/include/utils/event_loop.hpp
@@ -3,12 +3,11 @@
 #include "noncopyable.hpp"
 
 namespace uranus::utils {
-//这个基类定义事件循环的接口
-class EventLoop : public Noncopyable
-{
+// 这个基类定义事件循环的接口
+class EventLoop : public Noncopyable {
 public:
-    EventLoop()                          = default;
-    virtual ~EventLoop()                 = 0;
+    EventLoop()          = default;
+    virtual ~EventLoop() = 0;
 
     virtual auto addEvent(int fd) -> int = 0;
     virtual auto delEvent(int fd) -> int = 0;
